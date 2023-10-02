c657916730d58b050b12fcf8c1bf5bb7be07b85a
RicardoLopera <rloperav@unal.edu.co>
2023-10-02 07:52:11 -0500

    relax and take notes

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -15,7 +15,7 @@ public class Pais {
 		Pais paisMasRepetido = null;
 		for (int i = 0; i<n;i++) {
 			int count = 0;
-			for (int j = 0; j<n;i++) {
+			for (int j = 0; j<n;j++) {
 				if (listado.get(i)== listado.get(j)) {
 					count++;
 				}
